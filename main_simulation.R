7dc608242054927dd187ec8912b5f10452a5da3d
ZhaotongL <lin00374@umn.edu>
2022-03-22 21:58:46 -0500

    Add more functions for RAPS and NOME assumption

--- main_simulation.R
+++ main_simulation.R
@@ -4,6 +4,16 @@ library(mixIE)
 library(MRcML)
 source('./mr_egger_func.R')
 
+Isq <- function(y,s){
+  k = length(y)
+  w = 1/s^2; sum.w = sum(w)
+  mu.hat = sum(y*w)/sum.w
+  Q = sum(w*(y-mu.hat)^2)
+  Isq = (Q - (k-1))/Q
+  Isq = max(0,Isq)
+  return(Isq)
+}
+
 two_runif <- function(m,min1,min2,max1,max2){
   y <- runif(m, 0, max1-min1+max2-min2)
   y[which(y<(max1-min1))] = min1+y[which(y<(max1-min1))]
@@ -11,6 +21,7 @@ two_runif <- function(m,min1,min2,max1,max2){
   return(y)
 }
 
+#Simulation(a) w/ balanced pleiotropy
 generate_gwas_BP_1 <- function(seed,n,m,theta,K){
   set.seed(seed)
   gx = lapply(1:m,function(x){rbinom(n,2,0.3)})
@@ -20,6 +31,7 @@ generate_gwas_BP_1 <- function(seed,n,m,theta,K){
   bug = rep(0,m)
   if(K>=1){
     byg[1:K] = rnorm(K,0,0.1)
+#    bug[1:K] = runif(K,0,b)  ## correlated pleiotropy in Supplementary S8
   }
   eu = rnorm(n,0,1)
   ex = rnorm(n,0,1)
@@ -44,6 +56,7 @@ generate_gwas_BP_1 <- function(seed,n,m,theta,K){
   return(list(byg=byg,bxg=bxg,betax=betax,sdx=sdx,betay=betay,sdy=sdy,heri=heri,gx=gx,are=are))
 }
 
+#Simulation(b) w/ balanced pleiotropy
 generate_gwas_BP_2 <- function(seed,n,m,theta,K){
   set.seed(seed)
   gx = lapply(1:m,function(x){rbinom(n,2,0.3)})
@@ -77,6 +90,7 @@ generate_gwas_BP_2 <- function(seed,n,m,theta,K){
   return(list(byg=byg,bxg=bxg,betax=betax,sdx=sdx,betay=betay,sdy=sdy,heri=heri,gx=gx,are=are))
 }
 
+#Simulation(a) w/ directional pleiotropy
 generate_gwas_DP_1 <- function(seed,n,m,theta,K){
   set.seed(seed)
   gx = lapply(1:m,function(x){rbinom(n,2,0.3)})
@@ -110,11 +124,46 @@ generate_gwas_DP_1 <- function(seed,n,m,theta,K){
   return(list(byg=byg,bxg=bxg,betax=betax,sdx=sdx,betay=betay,sdy=sdy,heri=heri,gx=gx,are=are))
 }
 
+#Simulation(b) w/ directional pleiotropy
 generate_gwas_DP_2 <- function(seed,n,m,theta,K){
   set.seed(seed)
   gx = lapply(1:m,function(x){rbinom(n,2,0.3)})
   gx = do.call(cbind,gx)
   bxg = two_runif(m,min1=-0.1,max1=-0.03,min2=0.1,max2=0.2)
+  byg = rep(0,m)
+  bug = rep(0,m)
+  if(K>=1){
+    byg[1:K] = rnorm(K,0.1,0.1)
+  }
+  eu = rnorm(n,0,1)
+  ex = rnorm(n,0,1)
+  ey = rnorm(n,0,1)
+  u = gx %*% bug + eu
+  x = gx %*% bxg + u + ex
+  y = theta*x + gx %*% byg + u + ey
+  heri=var(gx %*% bxg)/var(x)
+  
+  ind1 = 1:(n/2) #first half sample
+  ind2 = (n/2+1):n #second half sample
+  gwasx = lapply(1:m,function(i){lm1=lm(x[ind1]~gx[ind1,i]);
+  summary(lm1)$coefficients[2,1:2]})
+  betax = unname(unlist(lapply(gwasx,function(x){x[1]})))
+  sdx = unname(unlist(lapply(gwasx,function(x){x[2]})))
+  gwasy = lapply(1:m,function(i){lm2=lm(y[ind2]~gx[ind2,i]);
+  summary(lm2)$coefficients[2,1:2]})
+  betay = unname(unlist(lapply(gwasy,function(x){x[1]})))
+  sdy = unname(unlist(lapply(gwasy,function(x){x[2]})))
+  are = sum(betax^2)/sum((betax-mean(betax))^2)
+  
+  return(list(byg=byg,bxg=bxg,betax=betax,sdx=sdx,betay=betay,sdy=sdy,heri=heri,gx=gx,are=are))
+}
+
+#Simulation(c) w/ directional pleiotropy
+generate_gwas_DP_3 <- function(seed,n,m,theta,K){
+  set.seed(seed)
+  gx = lapply(1:m,function(x){rbinom(n,2,0.3)})
+  gx = do.call(cbind,gx)
+  bxg = runif(m,min=0.1,max=0.3)
   byg = rep(0,m)
   bug = rep(0,m)
   if(K>=1){
@@ -151,10 +200,14 @@ run_simulation_egger <- function(seed,n,m,theta,K,type){
     
     ivw = TwoSampleMR::mr_ivw(b_exp=s$betax,se_exp = s$sdx,b_out = s$betay,se_out = s$sdy)
     egger_oracle = mr_egger_ll(b_exp=s$betax,se_exp = s$sdx,b_out = s$betay,se_out = s$sdy,flip=0)
+    I2_oracle = Isq(b_exp/se_out,se_exp/se_out)
+    
     egger_default = TwoSampleMR::mr_egger_regression(b_exp,b_out,se_exp,se_out)
     b_exp_flip = egger_default$dat$b_exp
     b_out_flip = egger_default$dat$b_out
-  
+    I2_default = Isq(b_exp_flip/se_out,se_exp/se_out)
+    radial_default = egger_radial(format_radial(b_exp_flip, b_out_flip, se_exp, se_out),summary=FALSE,weights=1)
+    
     b_exp_random = b_exp_flip
     m_random_flip = sample(1:(m-1),size=1)
     random_flip = sample(1:m,size=m_random_flip,replace=FALSE)
@@ -170,7 +223,10 @@ run_simulation_egger <- function(seed,n,m,theta,K,type){
     CEMdp_result = mixIE_MA_DP(b_exp = s$betax,b_out=s$betay,se_exp = s$sdx,se_out = s$sdy,flip=1,n=n/2)
     CEMMA_b = CEMdp_result$mixIE_MA_theta; CEMMA_se = CEMdp_result$mixIE_MA_se; CEMMA_pval = CEMdp_result$mixIE_MA_pval; CEMMA_pi = CEMdp_result$mixIE_MA_pi;
     CEMMADP_b = CEMdp_result$mixIE_MA_DP_theta; CEMMADP_se = CEMdp_result$mixIE_MA_DP_se; CEMMADP_pval = CEMdp_result$mixIE_MA_DP_pval;
-     
+    
+    raps = mr.raps.all(b_exp=s$betax,se_exp = s$sdx,b_out = s$betay,se_out = s$sdy)
+    raps_pval = 2*pnorm(abs(raps$beta.hat/raps$beta.se),lower.tail=FALSE)
+    
     out = list()
     out$ivw_b = ivw$b
     out$ivw_pval = ivw$pval
